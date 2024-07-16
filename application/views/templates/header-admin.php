aef9b42b1e6ca064b19b5822eae868f33cac7eb8
nathanJuanito <154326723+nathanJuanito@users.noreply.github.com>
2024-07-16 07:59:33 +0300

    Update header-admin.php

--- application/views/templates/header-admin.php
+++ application/views/templates/header-admin.php
@@ -231,7 +231,6 @@
             </a>
             <div class="collapse" id="ui-basic">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/ui-features/buttons.html">Insertion</a></li>
                 <li class="nav-item"> <a class="nav-link" href="pages/ui-features/dropdowns.html">Listes</a></li>
               </ul>
             </div>
