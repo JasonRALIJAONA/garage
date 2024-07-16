3b980d157a081c9f247c1757a1b4636a72c8eb13
nathanJuanito <154326723+nathanJuanito@users.noreply.github.com>
2024-07-16 08:58:53 +0300

    Update date-paiement.php

--- application/views/date-paiement.php
+++ application/views/date-paiement.php
@@ -8,6 +8,30 @@
                       <label for="exampleInputUsername1">Date de paiement</label>
                       <input type="date" class="form-control" placeholder="Date">
                     </div>
+                    <div class="form-group">
+                      <label for="exampleInputUsername1">Numero de voiture</label>
+                      <input type="text" class="form-control" placeholder="Numero de voiture" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputUsername1">Type de service</label>
+                      <input type="text" class="form-control" placeholder="Type" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputUsername1">Date de debut</label>
+                      <input type="date" class="form-control" placeholder="date" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputUsername1">Date de fin</label>
+                      <input type="date" class="form-control" placeholder="date" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputUsername1">Date de paiement</label>
+                      <input type="date" class="form-control" placeholder="date" readonly>
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputUsername1">Prix</label>
+                      <input type="number" class="form-control" placeholder="Prix" readonly>
+                    </div>
                     <button type="submit" class="btn btn-primary mr-2">Modifier</button>
                   </form>
                   
